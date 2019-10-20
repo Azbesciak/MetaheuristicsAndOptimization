04e81f66143ef5bf1506de0f3f4871863662d543
Witold Kupś <witold.kups@interlan.pl>
2019-10-20 14:23:36 +0200

    Make symmetric weight matrix faster

--- src/main/kotlin/pl/poznan/put/mioib/algorithm/SymmetricWeightMatrix.kt
+++ src/main/kotlin/pl/poznan/put/mioib/algorithm/SymmetricWeightMatrix.kt
@@ -5,17 +5,23 @@ import pl.poznan.put.mioib.model.Instance
 class SymmetricWeightMatrix(
         private val instance: Instance,
         calculator: EdgeWeightCalculator
-): WeightMatrix {
-    private val matrix = DoubleArray(instance.size * instance.size) {
-        val from = it / instance.size
-        val to = it % instance.size
-        if (from >= to) 0.0
-        else calculator.calculate(instance.locations[from], instance.locations[to])
+) : WeightMatrix {
+    private val matrix = DoubleArray(instance.size * instance.size)
+
+    init {
+        instance.locations.forEachIndexed { i, from ->
+            (i + 1 until instance.size).forEach { j ->
+                val to = instance.locations[j]
+                val value = calculator.calculate(from, to)
+                matrix[i * instance.size + j] = value
+                matrix[j * instance.size + i] = value
+            }
+        }
     }
 
-    override fun get(from: Int, to: Int): Double {
-        val index = if (from >= to) (to * instance.size + from) else (from * instance.size + to)
-        return matrix[index]
-    }
+    // It is about 2x faster than checking whether from < to and then make an index,
+    // because of that we need to assign twice.
+    // But this assignment does not cost much - about 5% longer even for 5k locations on create time.
+    override fun get(from: Int, to: Int) = matrix[from * instance.size + to]
 
 }
