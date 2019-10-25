96cffe401477d276a0e9ffdd815d8c2c3c13c94d
yaxlie <m.lesnierowski@gmail.com>
2019-10-25 02:19:46 +0200

    Add saving summary to file

--- src/main/kotlin/pl/poznan/put/mioib/model/Summary.kt
+++ src/main/kotlin/pl/poznan/put/mioib/model/Summary.kt
@@ -1,8 +1,24 @@
 package pl.poznan.put.mioib.model
 
+import com.google.gson.Gson
+import java.io.File
+import java.lang.Exception
+import java.util.*
+
 data class Summary(
     val name: String,
-    val scoreEntries: MutableList<Score>,
+    val scoreEntries: MutableList<Score>?,
     val time: Double
 ) {
+    fun save() {
+        try {
+            val gson = Gson()
+            val jsonString = gson.toJson(this)
+
+            File("${this.name}_${Date().toString()}.result").writeText(jsonString)
+        }
+        catch (e: Exception){
+            print("Error: Couldn't save Summary\n$e")
+        }
+    }
 }
