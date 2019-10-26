4633a7a2dd93664f712821159baab61da1861655
Witold Kupś <witold.kups@interlan.pl>
2019-10-26 09:38:42 +0200

    Add local search mutator

---
+++ src/main/kotlin/pl/poznan/put/mioib/algorithm/mutators/ls/LocalSearchMutator.kt
@@ -0,0 +1,20 @@
+package pl.poznan.put.mioib.algorithm.mutators.ls
+
+import pl.poznan.put.mioib.algorithm.mutators.SolutionMutator
+import pl.poznan.put.mioib.algorithm.weight.SolutionEvaluator
+import pl.poznan.put.mioib.model.SolutionProposal
+import pl.poznan.put.mioib.swappedAt
+
+class LocalSearchMutator(
+        private val browser: NeighbourhoodBrowser
+) : SolutionMutator {
+    override fun mutate(solution: SolutionProposal, solutionEvaluator: SolutionEvaluator): SolutionProposal {
+        val (update) = browser.browse(solution.sequence, solutionEvaluator)
+        return SolutionProposal(
+                sequence = solution.sequence.swappedAt(update.from, update.to),
+                score = solution.score + update.scoreDelta
+        )
+    }
+
+    override fun canMutate() = true
+}
---
+++ src/test/kotlin/pl/poznan/put/mioib/algorithm/mutators/ls/LocalSearchMutatorTest.kt
@@ -0,0 +1,31 @@
+package pl.poznan.put.mioib.algorithm.mutators.ls
+
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.jupiter.api.Test
+import pl.poznan.put.mioib.algorithm.weight.SolutionEvaluator
+import pl.poznan.put.mioib.model.SolutionProposal
+import kotlin.test.assertEquals
+
+internal class LocalSearchMutatorTest {
+
+    @Test
+    fun shouldUseBestBrowserSolution() {
+        val firstQuery = intArrayOf(1, 2, 3, 4)
+        val secondQuery = intArrayOf(1, 2, 4, 3)
+        val evaluator = mock<SolutionEvaluator> {}
+        val browser = mock<NeighbourhoodBrowser> {
+            on { browse(firstQuery, evaluator) } doReturn (listOf(DeltaUpdate(2, 3, 10.0)))
+            on { browse(secondQuery, evaluator) } doReturn (listOf(DeltaUpdate(1, 2, 15.0)))
+        }
+        val mutator = LocalSearchMutator(browser)
+        assertEquals(
+                SolutionProposal(intArrayOf(1, 2, 4, 3), 30.0),
+                mutator.mutate(SolutionProposal(firstQuery, 20.0), evaluator)
+        )
+        assertEquals(
+                SolutionProposal(intArrayOf(1, 4, 3, 3), 25.0),
+                mutator.mutate(SolutionProposal(secondQuery, 10.0), evaluator)
+        )
+    }
+}
