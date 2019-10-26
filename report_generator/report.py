12d8f80a80ae12b55e5cab71526f9ba26fec1425
yaxlie <m.lesnierowski@gmail.com>
2019-10-27 01:36:33 +0200

    Add latex document templates

--- report_generator/report.py
+++ report_generator/report.py
@@ -7,10 +7,15 @@ from glob import glob
 import charts
 from io import StringIO
 import shutil
+import templates
 
 def generate():
     output = StringIO()
 
+    # Put document intro
+    print(templates.INTRO, file=output)
+
+    # Generate charts
     for f in glob('{}/*.sum'.format(args.path), recursive=True):
         with open(f) as json_file:
             summary = json.load(json_file)
@@ -19,9 +24,14 @@ def generate():
             summary_report = charts.SummaryReport('summary', summary).generate()
             print(summary_report, file=output)
 
+    # Put document ending
+    print(templates.END, file=output)
+
+    # Save output as Latex document
     with open('report.tex', 'w') as fd:
         output.seek(0)
         shutil.copyfileobj(output, fd)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', type=Path,
---
+++ report_generator/templates.py
@@ -0,0 +1,32 @@
+INTRO = R'''
+\documentclass{article}
+\usepackage{polski}
+\usepackage[utf8]{inputenc} 
+\usepackage{amsmath}
+\usepackage{float}
+\usepackage[OT4]{fontenc} 
+\usepackage{enumitem}
+\usepackage{graphicx,color}
+\usepackage{url} 
+\usepackage[pdftex,hyperfootnotes=false,pdfborder={0 0 0}]{hyperref}
+
+\usepackage{float}
+
+\title{Metaheurystyki i Obliczenia Inspirowane Biologicznie}
+\author{Witold Kupś & Marcin Leśnierowski}
+\date{\footnotesize\today}
+
+\usepackage{natbib}
+\usepackage{graphicx}
+
+\begin{document}
+
+\maketitle
+\tableofcontents
+
+\newpage
+'''
+
+END=R'''
+\end{document}
+'''
