4a79ae498d5889b7174889121f836551fb1960cf
Witold Kupś <witold.kups@interlan.pl>
2019-10-26 15:19:12 +0200

    Use min duration instead of epsilon in measurement

--- src/main/kotlin/pl/poznan/put/mioib/benchmark/measurement.kt
+++ src/main/kotlin/pl/poznan/put/mioib/benchmark/measurement.kt
@@ -6,12 +6,12 @@ import java.time.Duration
 inline fun measureTime(
         minRetries: Int = 0,
         warmUp: Int = 0,
-        epsilon: Duration = Duration.ofMillis(1),
+        minDuration: Duration = Duration.ofMillis(1),
         action: () -> Unit
 ): Double {
     if (warmUp > 0)
         makeWarmUp(warmUp, action)
-    val epsilonInNanos = epsilon.toNanos() * 100
+    val minDurationInNanos = minDuration.toNanos()
     val start = System.nanoTime()
     var endTime: Long
     var counter = 0
@@ -19,7 +19,7 @@ inline fun measureTime(
         action()
         ++counter
         endTime = System.nanoTime()
-    } while (endTime - start < epsilonInNanos || counter < minRetries)
+    } while (endTime - start < minDurationInNanos || counter < minRetries)
     return (endTime - start) / counter.toDouble() / 1e6 // from nano to millis
 }
 
