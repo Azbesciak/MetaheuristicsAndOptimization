e490453b1087fefb3c7fa61b9bf16b997c2e491a
Witold Kupś <witold.kups@interlan.pl>
2019-10-26 15:20:07 +0200

    Add runner

--- src/main/kotlin/pl/poznan/put/mioib/Main.kt
+++ src/main/kotlin/pl/poznan/put/mioib/Main.kt
@@ -1,12 +1,64 @@
 package pl.poznan.put.mioib
 
+import pl.poznan.put.mioib.algorithm.mutators.MergedMutator
+import pl.poznan.put.mioib.algorithm.mutators.ls.GreedyNeighbourhoodBrowser
+import pl.poznan.put.mioib.algorithm.mutators.ls.LocalSearchMutator
+import pl.poznan.put.mioib.algorithm.mutators.random.RandomMutator
+import pl.poznan.put.mioib.algorithm.stopcondition.AnyStopCondition
+import pl.poznan.put.mioib.algorithm.stopcondition.NotImprovingSolutionStopCondition
+import pl.poznan.put.mioib.algorithm.weight.*
 import pl.poznan.put.mioib.benchmark.measureTime
+import pl.poznan.put.mioib.model.Instance
+import pl.poznan.put.mioib.model.SolutionProposal
+import pl.poznan.put.mioib.parser.BestScoresReader
+import pl.poznan.put.mioib.parser.InstanceParser
+import pl.poznan.put.mioib.parser.SolutionParser
+import pl.poznan.put.mioib.reader.InstanceSolutionReader
+import pl.poznan.put.mioib.solver.Solver
+import kotlin.random.Random
 
 fun main(args: Array<String>) = ProgramExecutor {
-    val values = IntArray(instanceSize) { it }
-    val averageTime = measureTime(
-            minRetries, warmUp, timeOut
-    ) { values.shuffle() }
-    println("TIME: $averageTime ns/instance")
+    val solutions = BestScoresReader.read(solutionValuesPath)
+    val instances = InstanceSolutionReader(
+            InstanceParser,
+            SolutionParser { solutions[it] }
+    ).read(instancePath)
+    instances.forEach {
+        val instance = it.instance
+        val evaluator = SymmetricSolutionEvaluator(SymmetricWeightMatrix(instance, Euclides2DWeightCalculator))
+        val isBetter = MIN_SOLUTION
+        val lsBrowser = GreedyNeighbourhoodBrowser(MIN_OR_EQUAL_SOLUTION_VALUE)
+        val collectedSolutions = mutableListOf<SolutionProposal>()
+        val random = Random(randomSeed)
+        val averageTime = measureTime(minRetries, warmUp, minDuration) {
+            val solution = solve(lsBrowser, isBetter, instance, evaluator) { random }
+            if (collectedSolutions.size < solutionsToCollect)
+                collectedSolutions += solution
+        }
+        val stats = collectedSolutions.stream().mapToDouble { it.score }.summaryStatistics()
+        println("${it.name} | TIME: $averageTime ns/instance | avgScore: ${stats.average} | bestScore: ${stats.min} | worstScore: ${stats.max} | instanceBest: ${solutions[instance.name]}")
+    }
 }.main(args)
 
+private fun Params.solve(
+        lsBrowser: GreedyNeighbourhoodBrowser,
+        isBetter: SolutionComparator,
+        instance: Instance,
+        evaluator: SymmetricSolutionEvaluator,
+        random: () -> Random
+): SolutionProposal {
+    val mutator = MergedMutator(
+            RandomMutator(random(), 1),
+            LocalSearchMutator(lsBrowser)
+    )
+    val stopCondition = prepareStopCondition(isBetter)
+    return Solver.solve(
+            instance = instance, stopCondition = stopCondition,
+            evaluator = evaluator, isBetter = isBetter,
+            mutator = mutator
+    )
+}
+
+private fun Params.prepareStopCondition(isBetter: SolutionComparator) = AnyStopCondition(
+        NotImprovingSolutionStopCondition(notImprovingSolutions, isBetter)
+)
--- src/main/kotlin/pl/poznan/put/mioib/ProgramExecutor.kt
+++ src/main/kotlin/pl/poznan/put/mioib/ProgramExecutor.kt
@@ -3,24 +3,43 @@ package pl.poznan.put.mioib
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.validate
 import com.github.ajalt.clikt.parameters.types.int
 import java.time.Duration
 
 class ProgramExecutor(private val task: Params.() -> Unit) : CliktCommand() {
-    private val instanceSize by option(help = "Instance size").int().default(100000).validate { it > 0 }
+    private val instancePath by option(help = "Instance path without extension; may be shortcut").required()
+    private val solutionValuesPath by option(help = "Solutions' values path").required()
     private val minRetries by option(help = "min number of retries").int().default(100).validate { it >= 0 }
     private val warmUp by option(help = "warm up iterations").int().default(100).validate { it >= 0 }
-    private val timeOut by option(help = "minimum execution time").default("PT1.0S")
+    private val minDuration by option(help = "minimum execution time").default("PT1.0S")
+    private val notImprovingSolutions by option(help = "max number of not improving solutions")
+            .int().default(100).validate { it >= 0 }
+    private val randomSeed by option(help = "random seed value").int().default(1234)
+    private val solutionsToCollect by option(help = "number of solutions to collect").int().default(10)
 
     override fun run() {
-        Params(instanceSize, minRetries, warmUp, timeOut = Duration.parse(timeOut.toUpperCase())).task()
+        Params(
+                instancePath = instancePath,
+                solutionValuesPath = solutionValuesPath,
+                minRetries = minRetries,
+                warmUp = warmUp,
+                minDuration = Duration.parse(minDuration.toUpperCase()),
+                notImprovingSolutions = notImprovingSolutions,
+                randomSeed = randomSeed,
+                solutionsToCollect = solutionsToCollect
+        ).task()
     }
 }
 
 data class Params(
-        val instanceSize: Int,
+        val instancePath: String,
+        val solutionValuesPath: String,
         val minRetries: Int,
         val warmUp: Int,
-        val timeOut: Duration
+        val minDuration: Duration,
+        val notImprovingSolutions: Int,
+        val randomSeed: Int,
+        val solutionsToCollect: Int
 )
