441876fdafb7e0963f0d3b87dc89c7133324bec0
Witold Kupś <witold.kups@interlan.pl>
2019-10-26 08:49:59 +0200

    Move shuffle to utils

--- src/main/kotlin/pl/poznan/put/mioib/Main.kt
+++ src/main/kotlin/pl/poznan/put/mioib/Main.kt
@@ -1,22 +1,12 @@
 package pl.poznan.put.mioib
 
 import pl.poznan.put.mioib.benchmark.measureTime
-import java.util.concurrent.ThreadLocalRandom
 
 fun main(args: Array<String>) = ProgramExecutor {
     val values = IntArray(instanceSize) { it }
     val averageTime = measureTime(
             minRetries, warmUp, timeOut
-    ) { values.shuffleArray() }
+    ) { values.shuffle() }
     println("TIME: $averageTime ns/instance")
 }.main(args)
 
-fun IntArray.shuffleArray() {
-    val rnd = ThreadLocalRandom.current()
-    (size - 1 downTo 1).forEach { i ->
-        val index = rnd.nextInt(i + 1)
-        val a = get(index)
-        set(index, get(i))
-        set(i, a)
-    }
-}
--- src/main/kotlin/pl/poznan/put/mioib/utils.kt
+++ src/main/kotlin/pl/poznan/put/mioib/utils.kt
@@ -1,7 +1,29 @@
 package pl.poznan.put.mioib
 
+import java.util.concurrent.ThreadLocalRandom
+
 val emptySignsRegex = "\\s+".toRegex()
 
 fun String.splitByWhiteSpace() = trim().split(emptySignsRegex)
 
-val String.looksLikePositiveNumber get() = trimStart().firstOrNull()?.isDigit() == true
+val String.looksLikePositiveNumber
+    get() = trimStart().firstOrNull()?.isDigit() == true
+
+fun IntArray.swappedAt(first: Int, second: Int) =
+        copyOf().also { it.swap(first, second) }
+
+fun IntArray.swap(first: Int, second: Int) {
+    val temp = get(first)
+    set(first, get(second))
+    set(second, temp)
+}
+
+fun IntArray.shuffled(random: ThreadLocalRandom = ThreadLocalRandom.current()) =
+        copyOf().also { it.shuffle(random) }
+
+fun IntArray.shuffle(random: ThreadLocalRandom = ThreadLocalRandom.current()) {
+    (size - 1 downTo 1).forEach { i ->
+        val index = random.nextInt(i + 1)
+        swap(i, index)
+    }
+}
