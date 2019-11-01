328c4b4987f7243c23e6a5c04804e176dfef9958
yaxlie <m.lesnierowski@gmail.com>
2019-11-01 10:31:38 +0100

    Move summary save function to utils

--- src/main/kotlin/pl/poznan/put/mioib/report/Summary.kt
+++ src/main/kotlin/pl/poznan/put/mioib/report/Summary.kt
@@ -13,25 +13,4 @@ data class Summary(
         val averageTime: Double,
         val score: Score,
         val attempts: List<Attempt> = listOf()
-) {
-
-
-
-    fun save(dir: String="summary"): String? {
-        return try {
-            val gson = GsonBuilder().setPrettyPrinting().create()
-            val jsonString = gson.toJson(this)
-            val filename = "${this.name}.sum"
-            val directory = File(dir)
-
-            directory.mkdirs()
-
-            File(directory, filename).writeText(jsonString)
-            "$directory/$filename"
-        }
-        catch (e: Exception){
-            print("Error: Couldn't save Summary\n$e")
-            null
-        }
-    }
-}
+)
